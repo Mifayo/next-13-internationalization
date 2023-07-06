96d4f1ceac10a3514a376b4adc4c8884f03f14a8
Faysal MISSI <faysal.missi@gmail.com>
2023-07-06 14:39:40 +0100

    p to h1

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   const t = useTranslations('Index');
   return (
     <div>
-      <p>{t('title')}</p>
+      <h1 className="text-3xl font-bold underline">{t('title')}</h1>
     </div>
   )
 }
